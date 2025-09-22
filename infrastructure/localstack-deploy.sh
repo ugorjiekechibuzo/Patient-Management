dc937b2a706cac2163ceab047489d241834cafe2
ugorji eke <ugorjiekechibuzo@gmail.com>
2025-09-22 21:11:18 +0100

    ReadMe

--- infrastructure/localstack-deploy.sh
+++ infrastructure/localstack-deploy.sh
@@ -9,6 +9,6 @@ aws --endpoint-url=http://localhost:4566 cloudformation deploy \
     --stack-name patient-management \
     --template-file "./cdk.out/localstack.template.json"
 
-aws --endpoint-url:http://localhost:4566 elbv2 describe-load-balancers \
+aws --endpoint-url=http://localhost:4566 elbv2 describe-load-balancers \
     --query "LoadBalancers[0].DNSName" --output text
 
